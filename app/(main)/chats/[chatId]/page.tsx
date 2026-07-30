a8c7754a31bad79805fd9d85b165060e4e559afd
Candihcar <gvrpaofficial@gmail.com>
2026-07-30 23:21:52 +0500

    Delete app directory

--- app/(main)/chats/[chatId]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ChatView } from '@/components/Chat/ChatView'
-
-export default function ChatPage({ params }: { params: { chatId: string } }) {
-  return <ChatView chatId={params.chatId} />
-}
